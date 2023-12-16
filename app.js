c437f7df06ca5d3a73b946e725885109b74b127b
ratigugutishvili <ratigugutishvili2004@mail.ru>
2023-12-16 21:35:30 +0400

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 8080;
 
 // Shared state array
 let sharedState = ["John", "Jane", "Doe","rati",'eka'];
